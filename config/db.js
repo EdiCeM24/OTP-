2bba65fde027ae7c7d6e47893b38daf8d7950783
Marcus Edidiong Cletus <edi_mars@outlook.com>
2026-03-22 11:32:36 +0100

    Add changes made in db.

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 import { Sequelize } from 'sequelize';
+import { DATABSE_NAME, DATABSE_USERNAME, DATABSE_PASSWORD } from './env.js';
 
 // Create connection
-const sequelize = new Sequelize( 'myadmin', 'root', '', {
+const sequelize = new Sequelize( DATABSE_NAME, DATABSE_USERNAME, DATABSE_PASSWORD, {
   host: 'localhost',
   dialect:'mysql'
 });
--- config/env.js
+++ config/env.js
@@ -2,4 +2,15 @@ import { config } from 'dotenv';
 
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local`});
 
-export const { PORT, NODE_ENV, JWT_SECRET_KEY, JWT_REFRESH_SECRET_KEY, JWT_EXPIRES_IN, EMAIL_USER, EMAIL_PASS } = process.env
+export const { 
+  PORT, 
+  NODE_ENV, 
+  JWT_SECRET_KEY, 
+  JWT_REFRESH_SECRET_KEY, 
+  JWT_EXPIRES_IN, 
+  EMAIL_USER, 
+  EMAIL_PASS,
+  DATABSE_NAME,
+  DATABSE_USERNAME,
+  DATABSE_PASSWORD,
+} = process.env
